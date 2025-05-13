9f4c9618a8ef7b839082f1c2ac85a1d353d6dee6
naveen3830 <naveenr3830@gmail.com>
2025-05-13 17:41:03 +0530

    add basic fastapi code

---
+++ fastapi/examples/basic1.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+class Main():
+    def __init__(self):
+        self.app=FastAPI()
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+        )
+        
+        @self.app.get("/")
+        async def root():
+            return {"message":"The app is up and running"}
+        
+main_app=Main()
+app=main_app.app
